ce939eb82a2ff3ce8b4289bddd90eef48df16115
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-07-29 17:26:01 +0300

    feat: add greeting.js

---
+++ src/commands/greeting.js
@@ -0,0 +1,13 @@
+import { argv, stdin as input, stdout as output } from "node:process";
+import { createInterface } from "node:readline";
+
+export const greeting = async () => {
+  const enteredUserName = argv[2].split("=")[1];
+  const rl = createInterface({
+    input,
+    output,
+  });
+  console.log(`Welcome to the File Manager, ${enteredUserName}!`);
+
+  rl.close();
+}
