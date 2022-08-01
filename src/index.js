c8a0a04dc07121fd086bd1de172de7f4e936400c
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-08-01 23:40:47 +0300

    fix: correct code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-
-import { greeting } from "./commands/greeting.js";
 import { showCurrentDirectory } from "./commands/showCurrentDir.js";
 import { list } from "./commands/ls.js";
-greeting();
-showCurrentDirectory();
-// list();
+import { argv, stdin as input, stdout as output } from "node:process";
+import { createInterface } from "node:readline";
 
+showCurrentDirectory();
+
+const enteredUserName = argv[2].split("=")[1];
+const rl = createInterface({
+  input,
+  output,
+});
+console.log(`Welcome to the File Manager, ${enteredUserName}!`);
+
+rl.on("line", (command) => {
+  const input = command.trim();
+
+  switch (input) {
+    case ".exit":
+      console.log(`Thank you for using File Manager, ${enteredUserName}!`);
+      rl.close();
+      break;
+    case "ls":
+      (async () => {
+        try {
+          const files = await list();
+          console.log(files);
+          showCurrentDirectory();
+        } catch (err) {
+          throw err;
+        }
+      })();
+
+    default:
+      break;
+  }
+});
+rl.on("close", () => {
+  console.log(`Thank you for using File Manager, ${enteredUserName}!`);
+});
